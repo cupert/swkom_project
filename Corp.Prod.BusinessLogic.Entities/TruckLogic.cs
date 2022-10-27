a06b5e4e604dbc8d52444ac9a7916c847213b7bf
cupert <76869929+cupert@users.noreply.github.com>
2022-10-27 14:04:46 +0200

    BL project erstellt

---
+++ Corp.Prod.BusinessLogic.Entities/TruckLogic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Corp.Prod.BusinessLogic.Interfaces;
+
+namespace Corp.Prod.BusinessLogic.Entities
+{
+    internal class TruckLogic:ITruckLogic
+    {
+    }
+}
---
+++ Corp.Prod.BusinessLogic.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Corp.Prod.BusinessLogic.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
