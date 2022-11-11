2d8c1357aebe917358a6e44c607557217f4227fc
Mjojoe <moritz.fenzl@aon.at>
2022-11-11 15:41:40 +0100

    bl -> DA mapping fertig?

--- Corp.Prod.BusinessLogic.Entities/MapperProfile.cs
+++ Corp.Prod.BusinessLogic.Entities/MapperProfile.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using Corp.Prod.Area.BusinessLogic.Entities.Converters;
-using Corp.Prod.DataAccess.Entities;
+using Corp.Prod.BusinessLogic.Entities.Converters;
 
 namespace Corp.Prod.BusinessLogic.Entities
 {
@@ -14,7 +14,31 @@ namespace Corp.Prod.BusinessLogic.Entities
         public MapperProfile()
         {
             CreateMap<Hop, DataAccess.Entities.Hop>()
-                .ForMember(d => d.LocationCoordinates, opt => opt.ConvertUsing(new GeoPointConverter(), src => src.LocationCoordinates));
+                .ForMember(d => d.LocationCoordinates, opt => opt.ConvertUsing(new GeoPointConverter(), src => src.LocationCoordinates))
+                .ReverseMap();
+
+            CreateMap<HopArrival, DataAccess.Entities.HopArrival>()
+                .ReverseMap();
+
+            CreateMap<Parcel, DataAccess.Entities.Parcel>()
+                .ReverseMap();
+
+            CreateMap<Recipient, DataAccess.Entities.Recipient>()
+                .ReverseMap();
+
+            CreateMap<Transferwarehouse, DataAccess.Entities.Transferwarehouse>()
+                .ForMember(d=>d.Region, opt =>opt.ConvertUsing(new GeoJsonConverter(), src=>src.RegionGeoJson))
+                .ReverseMap();
+
+            CreateMap<Truck, DataAccess.Entities.Truck>()
+                .ForMember(d=>d.Region,opt=>opt.ConvertUsing(new GeoJsonConverter(),src=>src.RegionGeoJson))
+                .ReverseMap();
+
+            CreateMap<Warehouse, DataAccess.Entities.Warehouse>()
+                .ReverseMap();
+
+            CreateMap<WarehouseNextHops, DataAccess.Entities.WarehouseNextHops>()
+                .ReverseMap();
         }
     }
 }
