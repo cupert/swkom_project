9be79246daa1a26ce32758d7e94d1536b854aeb9
Mjojoe <moritz.fenzl@aon.at>
2022-11-18 15:07:44 +0100

    fixed some stuff

--- Corp.Prod.Area.DataAccess.Sql/HopRepository.cs
+++ Corp.Prod.Area.DataAccess.Sql/HopRepository.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Corp.Prod.DataAccess.Entities;
 using Corp.Prod.DataAccess.Interfaces;
 
 namespace Corp.Prod.DataAccess.Sql
 {
     public class HopRepository : IHopRepository
     {
+        public Hop Create(Hop h)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hop GetHopByCode(string code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hop Update(Hop h)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
